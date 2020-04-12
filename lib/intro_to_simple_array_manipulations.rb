15b836261bc050e487426f1e48668cb831b3b3a7
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 14:45:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-#Add the new element to the 4th index of the array
+#Destructively add the new_element to the 4th index of the array
 def using_insert(array, new_element)
   array.insert(3,new_element)
 end
